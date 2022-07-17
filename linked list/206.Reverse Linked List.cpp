c80d567ea7d8a582fc775a1554ec306982c6b880
Tan Tan <m5823779@gmail.com>
2022-07-18 00:00:16 +0800

    update problem 206, upload problem 21

--- linked list/206.Reverse Linked List.cpp
+++ linked list/206.Reverse Linked List.cpp
@@ -29,3 +29,32 @@ public:
     // time complexity: O(n)
     // space complexity: O(1)
 };
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    // method 2. two recursive 
+    ListNode* reverseList(ListNode* head) {
+        return solve(head, NULL);
+    }
+private:
+    ListNode* solve(ListNode* head, ListNode* previous) {
+        // break condition
+        if (!head) return previous;
+        ListNode* next = head->next;
+        head->next = previous;
+        return solve(next, head);
+    }
+    // time complexity: O(n)
+    // space complexity: O(n)
+};
