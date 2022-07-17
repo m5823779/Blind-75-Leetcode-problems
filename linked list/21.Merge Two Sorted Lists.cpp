4c86f23956e7ffebce5983ca78957d0ce9827aa9
Tan Tan <m5823779@gmail.com>
2022-07-18 02:19:46 +0800

    update problem 21

--- linked list/21.Merge Two Sorted Lists.cpp
+++ linked list/21.Merge Two Sorted Lists.cpp
@@ -40,7 +40,42 @@ public:
         if (list2) tail->next = list2;
         
         return dummy.next;
-        // time complexity: O(m + n)
+        // time complexity: O(min(m, n))
         // space complexity: O(m + n)
+    }
+};
+
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        // method 2: recursive 
+        //  merge(a, b)
+        //  return b, if a is NULL
+        //  return a, if b is NULL
+        //  a[0] + merge(a.next, b), if a.val < b.val
+        //  b[0] + merge(a, b.next), if a.val >= b.val
+        
+        // example:
+        // l1: [1, 3, 5, 6]
+        // l2: [2, 4]
+        // [1] + mergeTwoLists([3, 5, 6], [2, 4])
+        // [2] + mergeTwoLists([3, 5, 6], [4])
+        // [3] + mergeTwoLists([5, 6], [4])
+        // [4] + mergeTwoLists([5, 6])
+        // [5, 6] 
+        
+        if (!list1) return list2;
+        if (!list2) return list1;
+        if (list1->val < list2->val) {
+            list1->next = mergeTwoLists(list1->next, list2);
+            return list1;
+        }
+        else {
+            list2->next = mergeTwoLists(list1, list2->next);
+            return list2;
+        }
+        // time complexity: O(max(m, n))
+        // space complexity: O(max(m, n))
     }
 };
