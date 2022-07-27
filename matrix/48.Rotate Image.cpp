d5ef4b1f533e267ec000d4d83e00c8d92f112c43
tantan <m5823779@gmail.com>
2022-07-28 02:59:07 +0800

    update problem 48

--- matrix/48.Rotate Image.cpp
+++ matrix/48.Rotate Image.cpp
@@ -35,3 +35,38 @@ public:
         // space complexity: O(1)
     }
 };
+
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        // method 2: diagonal
+        //  1  2  3       9  6  3       7  4  1
+        //  4  5  6   =>  8  5  2   =>  8  5  2
+        //  7  8  9       7  4  1       9  6  3
+        
+        const int n = matrix.size();
+        
+        for (int i = 0; i < n - 1; ++i) {  // notice range !!!
+            for (int j = 0; j < n - 1 - i; ++j) {  // notice range !!!
+                // swap(matrix[i][j], matrix[n - 1 - j][n - 1 - i]);
+                Swap(matrix[i][j], matrix[n - 1 - j][n - 1 - i]);
+            }
+        }
+
+        reverse(matrix.begin(), matrix.end());
+        // Reverse(matrix.begin(), matrix.end());
+    }
+private:
+    void Swap(int& a, int& b) {
+        int tmp = a;
+        a = b;
+        b = tmp;
+    }
+    
+    void Reverse(vector<vector<int>>::iterator a, vector<vector<int>>::iterator b) {
+        for (auto first = a, last = b - 1; first < last; ++first, --last) swap(*first, *last);
+    }
+    // time complexity: O(n^2 / 2)
+    // space complexity: O(1)
+};
