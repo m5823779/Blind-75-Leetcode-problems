7cb6339c8eafac072d847f36f67ecbc4eb0107d2
tantan <m5823779@gmail.com>
2022-08-08 00:46:09 +0800

    update problem 647

--- string/647.Palindromic Substrings.cpp
+++ string/647.Palindromic Substrings.cpp
@@ -53,3 +53,45 @@ public:
     // time complexity: O(n^2)
     // space complexity: O(1)
 };
+
+
+class Solution {
+public:
+    int countSubstrings(string s) {        
+        // method 3: dynamic programming
+        // example: "aaab"
+        
+        //    j 0  1  2  3   
+        // i    a  a  a  b   |
+        // 0  a T  T  T  F   |
+        // 1  a    T  T  F   |
+        // 2  a       T  F   |
+        // 3  b          T   v
+        
+        // axxa
+        // i  j -> check substring (i + 1, j - 1) is substring
+        
+        // 1. create 2D dynamic programming memory [s.size(), s.size()] to store result (is palindromic or not)
+        // 2. dp[i][j] = (s[i] == s[j]) and (d[i + 1][j - 1] if i - j > 3 else True)
+        // 3. count how many true
+        
+        int ans = 0;
+        
+        const int col = s.size(), row = s.size();
+        vector<bool> t(col, false); 
+        vector<vector<bool>> dp(row, t);
+        
+        for (int i = 0; i < row; ++i) {
+            for (int j = 0; j <= i; ++j) {
+                if (s[i] == s[j]) {
+                    if (i - j + 1 <= 3) dp[j][i] = true;
+                    else { if (i > 1 && j < s.size() - 2) dp[j][i] = dp[j + 1][i - 1]; }
+                }
+                if (dp[j][i]) ans += 1;
+            }
+        }
+        return ans;
+    }
+    // time complexity: O(n^2)
+    // space complexity: O(n^2)
+};
