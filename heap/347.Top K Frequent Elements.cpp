d5590d3c0159fde64d2e31c2967189befa8f2198
tantan <m5823779@gmail.com>
2022-08-11 02:59:11 +0800

    update problem 347

--- heap/347.Top K Frequent Elements.cpp
+++ heap/347.Top K Frequent Elements.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        // method 1: hash table
+        // method 1: hash table + sorting
         // 1. create unordered map to count appearance times of each element
         // 2. sorting by appearance times
         // 3. choose top K frequent element
@@ -23,6 +23,32 @@ public:
         
         for(int i = 0; i < k; ++i) ans.push_back(tmp[i].first);
         
+        return ans;
+    }
+    // time complexity: O(nlogn)
+    // space complexity: O(n)
+};
+
+
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        // method 2: priority queue + max heap
+        // 1. create unorded map to count appearance times of each element
+        // 2. create priority queue, push unorded map into it
+        // 3. pop out minimum element if queue size > k
+        // 4. choose top K frequent element
+        unordered_map<int, int> count;
+        for(auto &num : nums) count[num]++;
+        
+        priority_queue<std::pair<int, int>> max_heap;
+        for (auto &i : count) max_heap.push({i.second, i.first});
+
+        vector<int> ans;
+        while(k--) {
+            ans.push_back(max_heap.top().second);
+            max_heap.pop();
+        }
         return ans;
     }
     // time complexity: O(nlogn)
