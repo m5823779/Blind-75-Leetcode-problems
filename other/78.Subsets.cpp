7846b7ff1c7f797e717d834b33cd6c78cd44c978
Tan Tan <tantan@Tan-Mac-mini.local>
2023-01-21 23:35:16 +0800

    update problem 78

--- other/78.Subsets.cpp
+++ other/78.Subsets.cpp
@@ -3,26 +3,19 @@ public:
     vector<vector<int>> subsets(vector<int>& nums) {
         // mehtod: recursion (backtracking)
         vector<int> sub_ans;
-        solve(nums, 0, sub_ans);
+        solve(nums, sub_ans, 0);
         return ans;
     }
-
 private:
     vector<vector<int>> ans;
-    void solve(vector<int>& nums, int cur, vector<int> sub_ans) {
-        // base case
-        if (cur >= nums.size()) {
-            ans.push_back(sub_ans);
-            return;
+    void solve(vector<int>& nums, vector<int> sub_ans, int cur) {
+        ans.push_back(sub_ans);
+
+        for (int i = cur; i < nums.size(); i++) {
+            sub_ans.push_back(nums[i]);
+            solve(nums, sub_ans, i + 1);
+            sub_ans.pop_back();
         }
-
-        // not-pick
-        solve(nums, cur + 1, sub_ans);
-
-        // pick
-        sub_ans.push_back(nums[cur]);
-        solve(nums, cur + 1, sub_ans);
-        sub_ans.pop_back();
     }
     // time complexity: O(n * 2^n)  // 2^n combination, each need O(n) TC
     // space complexity: O(n)
@@ -63,3 +56,36 @@ private:
     // time complexity: O(n * 2^n)  // 2^n combination, each need O(n) TC
     // space complexity: O(n)
 };
+
+
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        // mehtod: recursion (backtracking)
+        vector<int> sub_ans;
+        solve(nums, 0, sub_ans);
+        return ans;
+    }
+
+private:
+    vector<vector<int>> ans;
+    void solve(vector<int>& nums, int cur, vector<int> sub_ans) {
+        // base case
+        if (cur >= nums.size()) {
+            ans.push_back(sub_ans);
+            return;
+        }
+
+        // not-pick
+        solve(nums, cur + 1, sub_ans);
+
+        // pick
+        sub_ans.push_back(nums[cur]);
+        solve(nums, cur + 1, sub_ans);
+        sub_ans.pop_back();
+    }
+    // time complexity: O(n * 2^n)  // 2^n combination, each need O(n) TC
+    // space complexity: O(n)
+};
+
+
