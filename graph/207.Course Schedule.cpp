89b3eb3928272a40f934a19b433a2e44a338dccb
Tan Tan <tantan@Tan-Mac-mini.local>
2023-02-03 11:43:28 +0800

    update problem 207

--- graph/207.Course Schedule.cpp
+++ graph/207.Course Schedule.cpp
@@ -40,3 +40,86 @@ private:
     // time complexity: O(V + E)
     // space complexity: O(n)
 };
+
+
+// method 2:
+struct Graph {
+    int numVertex;
+    list<int> *adj;
+    Graph(int n) {
+        this->numVertex = n;
+        adj = new list<int>[n];
+    }
+    void addEdge(int x, int y) {
+        adj[x].push_back(y);
+    }
+};
+
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        // topological sort
+        visited = vector<int>(numCourses, 0);
+        graph = new Graph(numCourses);
+        
+        // create graph
+        for (vector<int> p : prerequisites) {
+            graph->adj[p[0]].push_back(p[1]);
+        }
+
+        // check any cycle appear 
+        for (int i = 0; i < numCourses; i++) {
+            if (haveCycle(i)) return false;
+        }
+        return true;
+    }
+private:
+    Graph* graph;
+    vector<int> visited;
+    bool haveCycle(int class_id) {
+        if (visited[class_id] == 1) return true;
+        if (visited[class_id] == 2) return false;
+        visited[class_id] = 1;
+
+        for (int n : graph->adj[class_id]) {
+            if (haveCycle(n)) return true;
+        }
+        visited[class_id] = 2;
+        return false;
+    }
+};
+
+
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        // method 2: topological sort (BFS)
+        unordered_map<int, vector<int>> graph;
+        unordered_map<int, int> indegree;
+        for (vector<int> p : prerequisites) {
+            graph[p[0]].push_back(p[1]);
+            indegree[p[1]]++;
+        }
+
+        queue<int> q;
+        vector<int> topological_sort;
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                q.push(i);
+            }
+        }
+
+        while(!q.empty()) {
+            int tmp = q.front();
+            q.pop();
+            topological_sort.push_back(tmp);
+            for (int n : graph[tmp]) {
+                indegree[n]--;
+                if (indegree[n] == 0) {
+                    q.push(n);
+                }
+            }
+        }
+        return numCourses == topological_sort.size();
+    }
+};
