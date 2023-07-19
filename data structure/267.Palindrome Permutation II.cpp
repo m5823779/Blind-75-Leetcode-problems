cc00bf0b2c498015c48b480276dbbf543d563495
Tan Tan <m5823779@gmail.com>
2023-07-20 00:47:19 +0800

    update problem 267

--- data structure/267.Palindrome Permutation II.cpp
+++ data structure/267.Palindrome Permutation II.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<string> generatePalindromes(string s) {
-        // method 1: brute force (backtracking)
+        // method 1: brute force (backtracking) (time out)
         sort(s.begin(), s.end());
         vector<string> ans;
         vector<bool> used(s.size(), false);
@@ -39,5 +39,57 @@ public:
             r--;
         }
         return true;
+    }
+};
+
+
+class Solution {
+public:
+    vector<string> generatePalindromes(string s) {
+        // method: backtracking
+        vector<string> ans;
+        string sub_ans;
+
+        // step 1: check is palindrom permutation or not
+        vector<int> counter(26, 0);
+        // count frequency of each char
+        for (char c : s) {
+            // pass space
+            if (c == ' ') continue;
+
+            // uppercase -> lowercase (A -> a)
+            int id = (c - 'a' + 32) % 32;
+            counter[id]++;
+        }
+
+        // check frequency of appear times is odd number
+        int appearOdd = 0;
+        for (int i = 0; i < 26; i++) {
+            // appear times is odd number
+            if (counter[i] % 2) {
+                appearOdd++;
+                sub_ans.push_back(i + 'a');
+                counter[i]--;
+            }
+        }
+        if (appearOdd > 1) return {};
+        
+        // step 2: backtracking
+        backtracking(s, counter, ans, sub_ans);
+        return ans;
+    }
+
+    void backtracking(const string& s, vector<int>& counter, vector<string>& ans, string sub_ans) {
+        if (sub_ans.size() == s.size()) {
+            ans.push_back(sub_ans);
+            return;
+        }
+        for (int i = 0; i < counter.size(); i++) {
+            if (counter[i] == 0) continue;
+            counter[i] -= 2;
+            char addChar = (char)(i + 'a');
+            backtracking(s, counter, ans, addChar + sub_ans + addChar);
+            counter[i] += 2;
+        }
     }
 };
